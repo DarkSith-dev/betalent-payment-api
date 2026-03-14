a3d03bbef4f8c8055bffc42a913ccd35ec66ed5c
Diego Alves Rodrigues <51385145+DarkSith-dev@users.noreply.github.com>
2026-03-14 12:41:43 -0300

    feat: configuração inicial com AdonisJS, MySQL, migrations e estrutura do projeto

---
+++ betalant-payments/database/migrations/1773456738250_create_transactions_table.ts
@@ -0,0 +1,29 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'transactions'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('client_id').unsigned().notNullable()
+      table.integer('gateway_id').unsigned().nullable()
+
+      table.string('external_id').nullable()
+
+      table.string('status').notNullable()
+
+      table.integer('amount').notNullable()
+
+      table.string('card_last_numbers', 4).nullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
